787303d5e1a75e8f0931905075381069e3a7e6e0
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-05-05 12:36:01 -0600

    final commit

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 11, "M", "Brown", "Kind", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Rowdy", 11, "M", "Brown", "Rowdy", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Tabitha", 11, "F", "White", "Aggressive", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 15, "M", "Brown", "Wise", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Melissa", 17, "M", "Black", "Was Docile", 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Grinch", 45, "M", "Green", "Conniving", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Wendy", 9, "F", "Tan", "Sweet", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES (null, 11, "M", "Brown", "Dead", 0);
